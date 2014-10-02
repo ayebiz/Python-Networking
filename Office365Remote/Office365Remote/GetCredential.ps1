bd63d8be107438cbd5608ede2d826c9412e9ae54
ayebiz <albert@ayebiz.net>
2014-10-02 12:06:32 -0700

    Add o365

---
+++ Office365Remote/Office365Remote/GetCredential.ps1
@@ -0,0 +1,1 @@
+$LiveCred = Get-Credential
---
+++ Office365Remote/Office365Remote/Office365Remote.py
@@ -0,0 +1,12 @@
+#Objective1: Run Powershell
+#Objective2: Connect to Office 365 remotely
+#Objective3: Create Shared mailbox
+#Objective4: Create User
+#Objective5: Create mailbox
+
+import subprocess
+import os
+#subprocess.call(["C:\Windows\System32\WindowsPowerShell\v1.0\powershell.exe", ".\"./GetCredential.ps1\";", "&hello"])
+#from subprocess import check_output
+#check_output("dir", shell=True)
+os.system('powershell.exe')
